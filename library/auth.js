b3eba8621b2d2f678fc6b2279b1c31699fb7837f
unknown <santhosh8857@gmail.com>
2021-12-09 05:34:22 +0530

    added token

--- library/auth.js
+++ library/auth.js
@@ -1,6 +1,11 @@
 // bcryptjs is pureJS and bcrypt has dependencies with c++
 const bcrypt = require("bcryptjs"); // for encrypting passwords
 
+const secret = "akslnfklenjpoerjqpd[jqwdif"; //random
+const JWT = require("jsonwebtoken"); // to create a token
+
+const JWTD = require("jwt-decode"); // to decode the token
+
 // this function will take the password from the client (req.body.password) and encrypt it.
 const hashing = async (value) => {
   try {
@@ -15,4 +20,46 @@ const hashing = async (value) => {
   }
 };
 
-module.exports = { hashing };
+// this function will take the provided password and encryted password from the DB and compare it.
+const hashCompare = async (currentPwd, hashedPwd) => {
+  try {
+    // this is return true or false.
+    return await bcrypt.compare(currentPwd, hashedPwd);
+  } catch (err) {
+    console.log("bcrypt compare error", err);
+  }
+};
+
+// this function will accept the username(email) and name(firstname) from the DB and create token.
+const createToken = async (username, name) => {
+  try {
+    // sign ("data", secret code, expiration time)
+    return JWT.sign(
+      {
+        username,
+        name,
+      },
+      secret,
+      {
+        expiresIn: "15m",
+      }
+    );
+  } catch (err) {
+    console.log("token creation error", err);
+  }
+};
+
+// this function will accept the token from the user and authenticate it. for this we are using jwt-decode package.
+const authenticate = async (token) => {
+  // calling JWTD to decode
+  const decode = JWTD(token);
+
+  // verfiy expiration time
+  if (Math.round(new Date() / 1000) <= decode.exp) {
+    return decode.username;
+  } else {
+    return false;
+  }
+};
+
+module.exports = { hashing, hashCompare, createToken, authenticate };
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,12 @@
 var express = require("express");
 var router = express.Router();
 const { mongodb, MongoClient, dbUrl } = require("../dbConfig");
-const { hashing } = require("../library/auth"); // importing for authentication
+const {
+  hashing,
+  hashCompare,
+  createToken,
+  authenticate,
+} = require("../library/auth"); // importing for authentication
 
 /* GET users */
 router.get("/", async (req, res) => {
@@ -35,10 +40,120 @@ router.post("/", async (req, res) => {
     req.body.activation = false;
     const data = await db.collection("users").insertOne(req.body);
 
+    // ---- //
+    // email to activate the account //
+    // ---- //
+
     res.send({ message: "Account created", data: data });
   } catch (err) {
     console.log(err);
     res.send({ message: "Error in connection", data: err });
+  } finally {
+    client.close();
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const client = await MongoClient.connect(dbUrl);
+
+  try {
+    const db = client.db("url-shortener");
+
+    const user = await db
+      .collection("users")
+      .findOne({ username: req.body.username }); // to get the user details.
+
+    // verify user
+    if (user) {
+      // verify activation
+      if (user.activation) {
+        // calling compare function to validate
+        const compare = await hashCompare(req.body.password, user.password);
+
+        // verify compare
+        if (compare) {
+          res.send({ message: "Login successfull!!!" });
+        } else {
+          res.send({ message: "Invalid username or password" });
+        }
+      } else {
+        console.log(user.activation);
+        res.send({ message: "Activate Account!!!" });
+      }
+    } else {
+      res.send({ message: "No user available" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send({ message: "Error in connection", data: err });
+  } finally {
+    client.close();
+  }
+});
+
+router.post("/forget-password", async (req, res) => {
+  const client = await MongoClient.connect(dbUrl);
+
+  try {
+    const db = client.db("url-shortener");
+
+    // fetching user details
+    const user = await db
+      .collection("users")
+      .findOne({ username: req.body.username });
+
+    // verify user
+    if (user) {
+      const token = await createToken(user.username, user.firstname);
+
+      // ----- //
+      // email with reset link includes token //
+      // ----- //
+
+      res.send({ token, message: "Reset link has been sent successfully!!!" });
+    } else {
+      res.send({ message: "Invalid username!!!" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send({ message: "Error in connection" });
+  }
+});
+
+// reset-password.  takes new password
+router.post("/reset-password/:token", async (req, res) => {
+  const client = await MongoClient.connect(dbUrl);
+
+  // ----- //
+  // should be navigated from the email
+  // ----- //
+
+  // call the authenticate func to validate the token and extract the email.
+  const tokenVerifiedUsername = await authenticate(req.params.token);
+  try {
+    // check token.
+    if (tokenVerifiedUsername) {
+      const db = client.db("url-shortener");
+
+      // password encryption
+      const hash = await hashing(req.body.password);
+      req.body.password = hash;
+
+      // updating new password
+      const data = await db
+        .collection("users")
+        .updateOne(
+          { username: tokenVerifiedUsername },
+          { $set: { password: req.body.password } }
+        );
+
+      res.send({ message: "Password updated successfully!!!" });
+    } else {
+      res.send({ message: "Link expired!!!" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send({ message: "Error in connection" });
   } finally {
     client.close();
   }
