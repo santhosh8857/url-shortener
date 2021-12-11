8fdb5348ffc2f249cd66866ebfbb5254bd5049f1
unknown <santhosh8857@gmail.com>
2021-12-11 21:55:10 +0530

    added nodemailer

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var router = express.Router();
+const nodemailer = require("nodemailer");
+const env = require("dotenv").config();
+const crypto = require("crypto");
 const { mongodb, MongoClient, dbUrl } = require("../dbConfig");
 const {
   hashing,
@@ -7,6 +10,19 @@ const {
   createToken,
   authenticate,
 } = require("../library/auth"); // importing for authentication
+const { ObjectId } = require("mongodb");
+
+// sender email details
+var transpoter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "urlshortener66@gmail.com",
+    pass: process.env.PWD,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
 /* GET users */
 router.get("/", async (req, res) => {
@@ -26,7 +42,7 @@ router.get("/", async (req, res) => {
 });
 
 // to create/register a new user
-router.post("/", async (req, res) => {
+router.post("/register", async (req, res) => {
   const client = await MongoClient.connect(dbUrl);
 
   try {
@@ -38,16 +54,54 @@ router.post("/", async (req, res) => {
 
     // adding user with encrypted password and activation status.
     req.body.activation = false;
+    req.body.emailToken = crypto.randomBytes(64).toString("hex");
     const data = await db.collection("users").insertOne(req.body);
 
-    // ---- //
-    // email to activate the account //
-    // ---- //
+    // verification email to user
+    var mailOptions = {
+      from: ' "Verfiy your email" <urlshortener66@gmail.com>',
+      to: req.body.username,
+      subject: "url-shortener : Verify your email",
+      html: `<h2>${req.body.firstname}! Thank you for registering to our application </h2>
+        <h4>Please click on the below link to verify your email...</h4?
+        <a href='http://${req.headers.host}/users/verify-email/${req.body.emailToken}'>link to verify your email</a>`,
+    };
+
+    // sending email
+    transpoter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log("email error: ", error);
+      } else {
+        console.log("Verification email sent!!!");
+      }
+    });
 
     res.send({ message: "Account created", data: data });
   } catch (err) {
     console.log(err);
     res.send({ message: "Error in connection", data: err });
+  } finally {
+    client.close();
+  }
+});
+
+router.get("/verify-email/:emailToken", async (req, res) => {
+  const client = await MongoClient.connect(dbUrl);
+
+  try {
+    const db = client.db("url-shortener");
+
+    const user = await db
+      .collection("users")
+      .updateOne(
+        { emailToken: req.params.emailToken },
+        { $set: { emailToken: null, activation: true } }
+      );
+
+    res.send({ message: "Account activated!!!" });
+  } catch (err) {
+    console.log(err);
+    res.send({ message: "Error in connection!!!" });
   } finally {
     client.close();
   }
@@ -91,6 +145,7 @@ router.post("/login", async (req, res) => {
   }
 });
 
+// req => username
 router.post("/forget-password", async (req, res) => {
   const client = await MongoClient.connect(dbUrl);
 
@@ -106,11 +161,27 @@ router.post("/forget-password", async (req, res) => {
     if (user) {
       const token = await createToken(user.username, user.firstname);
 
-      // ----- //
-      // email with reset link includes token //
-      // ----- //
+      // send reset email to user
+      var mailOptions = {
+        from: ' "Reset your password" <urlshortener66@gmail.com>',
+        to: user.username,
+        subject: "url-shortener : Reset your password",
+        html: `<h2>${user.firstname}! Thanks for using our application </h2>
+        <h4>Please click on the below link to reset your password...</h4?
+        <a href='http://${req.headers.host}/users/reset-password/${token}'>Reset link</a>`,
+      };
 
-      res.send({ token, message: "Reset link has been sent successfully!!!" });
+      // sending email
+      transpoter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log("email error: ", error);
+        } else {
+          res.send({
+            token,
+            message: "Reset link has been sent successfully!!!",
+          });
+        }
+      });
     } else {
       res.send({ message: "Invalid username!!!" });
     }
@@ -120,13 +191,13 @@ router.post("/forget-password", async (req, res) => {
   }
 });
 
+router.get("/reset-password/:token", async (req, res) => {
+  res.render("index", { title: "Get from email" });
+});
+
 // reset-password.  takes new password
 router.post("/reset-password/:token", async (req, res) => {
   const client = await MongoClient.connect(dbUrl);
-
-  // ----- //
-  // should be navigated from the email
-  // ----- //
 
   // call the authenticate func to validate the token and extract the email.
   const tokenVerifiedUsername = await authenticate(req.params.token);
