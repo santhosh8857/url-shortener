227fd14ccdc15511cc1dedd8c722e76e7e090299
unknown <santhosh8857@gmail.com>
2021-12-22 02:37:23 +0530

    updated frontend URL

--- routes/users.js
+++ routes/users.js
@@ -69,7 +69,7 @@ router.post("/register", async (req, res) => {
       subject: "url-shortener : Verify your email",
       html: `<h2>${user.firstname}! Thank you for registering to our application </h2>
         <h4>Please click on the below link to verify your email...</h4?
-        <a href='http://${process.env.URL}/verify-email/${user.emailToken}'>link to verify your email</a>`,
+        <a href='${process.env.URL}/verify-email/${user.emailToken}'>link to verify your email</a>`,
     };
 
     // sending email
@@ -181,7 +181,7 @@ router.post("/forget-password", async (req, res, next) => {
         subject: "url-shortener : Reset your password",
         html: `<h2>${user.firstname}! Thanks for using our application </h2>
         <h4>Please click on the below link to reset your password...</h4?
-        <a href='http://${process.env.URL}/reset-password/${token}'>Reset link</a>`,
+        <a href='${process.env.URL}/reset-password/${token}'>Reset link</a>`,
       };
 
       // sending email
